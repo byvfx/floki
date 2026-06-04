5d39c41d1a716eea36de81387d09aa6be04df2d0
Brandon Young <67305844+byvfx@users.noreply.github.com>
2026-06-03 22:09:56 -0700

    Remove scratch files

--- scratch_test.rs
+++
@@ -1,9 +0,0 @@
-use exr::prelude::*;
-use exr::block::reader::read_meta_data;
-use exr::meta::header::Header;
-
-fn main() {
-    let input = "X:/SuplexFX/TPLS2/206_206-0370/houdini/render/redSea_bty/v006/TPLS2_206_206-0370_render_v006.redSea_bty.1018.exr";
-    let meta_data = read_meta_data(input).unwrap();
-    println!("{:#?}", meta_data.headers);
-}
